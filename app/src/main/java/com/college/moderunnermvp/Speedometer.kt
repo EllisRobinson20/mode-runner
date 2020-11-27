275e17d50e92b3b86e2b801c9275b0442dec5e24
L EE <lee@MacBook-Pro.local>
2020-11-27 22:38:37 +0000

    finally a working service... the choice of implementation is an intent service. This is simple to implemement async tasks, keps code overhead low.

--- app/src/main/java/com/college/moderunnermvp/Speedometer.kt
+++ app/src/main/java/com/college/moderunnermvp/Speedometer.kt
@@ -1,49 +1,42 @@
 package com.college.moderunnermvp
 
-import android.content.ComponentName
-import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.IBinder
+import android.util.Log
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import kotlinx.android.synthetic.main.activity_speedometer.*
 
 class Speedometer : AppCompatActivity() {
-
-    var mService: SpeedometerService? = null
-    var isBound: Boolean = false
+    var distanceToRun: ArrayList<Int> = ArrayList()
+    var listView: ListView? = null
+    var adapter: ArrayAdapter<Int>? = null
+    var tag: String = "DistanceToRun"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_speedometer)
+        listView = findViewById(R.id.main_listview)
+        loadListView()
+    }
+    fun onStartClick(view: View) {
+        val textReceived = distance_input.text.toString().toInt()
+        distanceToRun.add(textReceived)
+        adapter?.notifyDataSetChanged()
+        startSpeedometerService(textReceived)
+    }
+    private fun loadListView() {
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, distanceToRun)
+        listView?.adapter = adapter
+    }
+    fun startSpeedometerService(text:Int) {
+        Log.i("App", "startSpeedometerMode")
         val intent = Intent(this, SpeedometerService::class.java)
-        //bindService(intent, myConnection, Context.BIND_AUTO_CREATE)
-    }
-
-    fun startSpeedometerService(view: View) {
-        /*val text_received = distance_input.text.toString().toInt()
-        val Count = mService?.getResult(text_received)
-        distance_output.text = "$Count"*/
-    }
-
-    /*private val myConnection = object : ServiceConnection {
-        override fun onServiceDisconnected(name: ComponentName?) {
-            isBound = false
-        }
-
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val binder = service as SpeedometerService.MyBinder
-            mService = binder.getService()
-            isBound = true
-        }
-
-    }*/
-
-
-   /* fun startSpeedometerService(view: View) {
-        var intent = Intent(this, SpeedometerService::class.java)
+        intent.putExtra(tag,text)
         startService(intent)
-    }*/
+    }
+
+
 }
